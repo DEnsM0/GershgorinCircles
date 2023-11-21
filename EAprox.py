5f516032c1a8afbde39421126536f70753e0efd3
D Mohylov <d.mohylov@gmail.com>
2023-11-21 10:26:26 +0100

    eigenvalues added

--- EAprox.py
+++ EAprox.py
@@ -3,42 +3,52 @@ from math import fabs
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 
-
-
-def GregsCircles(matrix):
-    if isSquare(matrix) != True:
+def GregsCircles(matrix, eigvals):
+    if not isSquare(matrix):
         print('Your input matrix is not square!')
         return []
+    
     circles = []
-    for x in range(0,len(matrix)):
+    for x in range(0, len(matrix)):
         radius = 0
         piv = matrix[x][x]
-        for y in range(0,len(matrix)):
+        for y in range(0, len(matrix)):
             if x != y:
                 radius += fabs(matrix[x][y])
-        circles.append([piv,radius])
+        circles.append([piv, radius])
+    
     return circles
 
-def plotCircles(circles):
+def plotCircles(circles, eigvals):
     index, radi = zip(*circles)
     Xupper = max(index) + np.std(index)
     Xlower = min(index) - np.std(index)
     Ylimit = max(radi) + np.std(index)
-    fig, ax = plt.subplots() 
-    ax = plt.gca()
-    ax.cla()
-    ax.set_xlim((Xlower,Xupper))
-    ax.set_ylim((-Ylimit,Ylimit))
-    plt.xlabel('Real Axis')
-    plt.ylabel('Imaginary Axis')
-    plt.title('Gershgorin circles')
-    for x in range(0,len(circles)):
-        circ = plt.Circle((index[x],0), radius = radi[x])
-        ax.add_artist(circ)
-    ax.plot([Xlower,Xupper],[0,0],'k--')
-    ax.plot([0,0],[-Ylimit,Ylimit],'k--')
-    fig.savefig('plotcircles.png')
     
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6), gridspec_kw={'height_ratios': [1, 6]})
+    
+    eigenvalue_text = '\n'.join([fr'$\lambda_{i} = {eigval}$' for i, eigval in enumerate(eigvals, start=1)])
+    ax1.text(0.5, 0.5, f'Eigenvalues:\n{eigenvalue_text}', va='center', ha='center', fontsize=12)
+    ax1.axis('off')
+    
+    ax2.set_xlim((Xlower, Xupper))
+    ax2.set_ylim((-Ylimit, Ylimit))
+    ax2.set_xlabel('Real')
+    ax2.set_ylabel('Imaginary')
+
+    for i, (eigval, label) in enumerate(zip(eigvals, [fr'$\lambda_{i}$' for i in range(1, len(eigvals) + 1)])):
+        circ = plt.Circle((index[i], 0), radius=radi[i], edgecolor='black', facecolor='none', linewidth=1)
+        ax2.add_artist(circ)
+        ax2.plot(eigval.real, eigval.imag, 'ro')
+        ax2.annotate(label, (eigval.real, eigval.imag), textcoords="offset points", xytext=(0,10), ha='center')
+
+    ax2.plot([Xlower, Xupper], [0, 0], 'k--')
+    ax2.plot([0, 0], [-Ylimit, Ylimit], 'k--')
+    
+    plt.tight_layout()
+    fig.savefig('plotcircles.png')
+    plt.show()
+
 def isSquare(m):
     cols = len(m)
     for row in m:
@@ -47,9 +57,10 @@ def isSquare(m):
     return True
 
 def main():
-    test = np.array([[10,-1,0,1],[0.2,8,0.2,0.2],[1,1,2,1],[-1,-1,-1,-11]])
-    temp = GregsCircles(test)
-    plotCircles(temp)
+    matrix = np.array([[2, 0.1, -0.5], [0.2, 3, 0.5], [-0.4, 0.1, 5]]) # enter your matrix
+    eigvals = LA.eigvals(matrix)
+    temp = GregsCircles(matrix, eigvals)
+    plotCircles(temp, eigvals)
 
 if __name__ == '__main__':
     main()
